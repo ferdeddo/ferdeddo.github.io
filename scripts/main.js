ee1b0e9448d0d002ede3e1e5f9602f73c0a11f1a
FERDEDDO <FERDEDDO@GMAIL.com>
2017-10-05 17:38:44 +0200

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+
+let monBouton = document.querySelector('button');
+
+function AfficherMasquer()
+{
+divInfo = document.getElementById('affich');
+ 
+if (divInfo.style.display == 'none')
+divInfo.style.display = 'block';
+
+else
+divInfo.style.display = 'none';
+document.getElementById('bouton').style.display = 'none';
+}
+
+function redirection()
+{
+divInfo = document.getElementById("barre")
+}
+
+setTimeout(function()
+{
+
+   alert('Hey le site xxxvidsxxx est trop bien. Viens dessus stp please')
+},10000);
